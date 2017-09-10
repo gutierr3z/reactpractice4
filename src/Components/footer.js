d255ef746edc39117031b07f55704d554746d184
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-10 13:57:36 -0400

    practice

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -11,6 +11,12 @@ const Footer = ( props ) => {
             <img src="./images/download.jpeg" />
             <br />
             Copyright 2017 React Practice Web Site. All rights reserved. World Wide Web.
+            <br />
+            <small>
+                Senior Webmaster: Cristian Satnic
+                <br />
+                Web Site Design, Construction & Management By Bob Inc.
+            </small>
             <img src="./images/alum.gif" width="600" />
         </footer>
     );
