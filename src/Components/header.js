12657ea2a9c0486a8d197eebf23a4417fb52b318
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-10 13:17:04 -0400

    practice

--- src/Components/header.js
+++ src/Components/header.js
@@ -5,7 +5,11 @@ const Header = ( props ) => {
     return (
         <header className="">
             <img src="./images/alum.gif" width="600" />
-            <h1><strong><i>Welcome!</i></strong></h1>
+            <h1>
+                <img src="./images/earth2.gif" />
+                <strong><i>Welcome!</i></strong>
+                <img src="./images/earth2.gif" />
+            </h1>
             <img src="./images/rainbow.gif" width="600" />
         </header>
     );
