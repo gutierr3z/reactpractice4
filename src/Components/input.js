4ab595901ecdf0f2bbb7ebe0e2e7fb224312e9b2
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-11 18:33:16 -0400

    following practice

---
+++ src/Components/input.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Input = ( props ) => {
+
+    return (
+        <div>
+            <input type="text" id="ipt_txt" /> &nbsp;
+            <input type="button" value="Enter" />
+        </div>
+    );
+};
+
+export default Input;
