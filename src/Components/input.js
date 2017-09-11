e5e9a11c3418cdeda620367803153ee4f8fbb338
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-11 18:47:36 -0400

    following practice

--- src/Components/input.js
+++ src/Components/input.js
@@ -2,14 +2,14 @@ import React from 'react';
 //==================================================
 const Input = ( props ) => {
 
-    const test = () => {
-        console.log( 'test test' );
-    };
+    function test() {
+        console.log( 'test' );
+    }
 
     return (
         <div>
             <input type="text" id="ipt_txt" /> &nbsp;
-            <input type="button" value={ props.btnVal } onChange = { this.test } />
+            <input type="button" value={ props.btnVal } onClick = { test } />
         </div>
     );
 };
