84b6d1437ac6cca482dffc14d2e743940f5ae8b8
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-11 18:21:28 -0400

    following practice

--- src/Components/header.js
+++ src/Components/header.js
@@ -3,15 +3,24 @@ import React from 'react';
 const Header = ( props ) => {
 
     return (
-        <header className="">
-            <img src="./images/alum.gif" width="600" />
-            <h1>
-                <img src="./images/earth2.gif" />
-                <strong><i>Welcome!</i></strong>
-                <img src="./images/earth2.gif" />
-            </h1>
-            <img src="./images/alum.gif" width="600" />
-        </header>
+        <div>
+            <header className="">
+                <img src="./images/alum.gif" width="600" />
+                <h1>
+                    <img src="./images/earth2.gif" />
+                    <strong><i>Welcome!</i></strong>
+                    <img src="./images/earth2.gif" />
+                </h1>
+                <img src="./images/alum.gif" width="600" />
+            </header>
+
+            <div className="oo-resolution">
+                <img src="./images/cube.gif" />
+                Please Set Your Screen Resolution To <span>800x600</span> For Optimal Viewing
+                <img src="./images/cube.gif" />
+            </div>
+            <img src="./images/rainbow.gif" width="600" />
+        </div>
     );
 };
 //==================================================
