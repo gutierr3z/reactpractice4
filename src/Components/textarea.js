0ebd0c6acdad429868c666b609e1a1286addd96d
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-12 19:24:44 -0400

    following practice

--- src/Components/textarea.js
+++ src/Components/textarea.js
@@ -3,13 +3,12 @@ import React, { Component } from 'react';
 const Textarea = ( props ) => {
 
     return (
-        
+
         <div>
             <textarea>
                 { props.hello }
             </textarea>
         </div>
-
     );
 };
 //==================================================
