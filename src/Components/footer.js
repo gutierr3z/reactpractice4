0adc4c877005bed3f188659da5ccde38343869ed
Juan Gutierrez <gutierr3z@gmail.com>
2017-09-15 23:22:48 -0400

    practice

--- src/Components/footer.js
+++ src/Components/footer.js
@@ -23,11 +23,12 @@ const Footer = ( props ) => {
                 </strong>
 
                 <br />
+                
                 <img src="./images/counter.gif" />
                 <br />
                 <img src="./images/download.jpeg" />
                 <br />
-                
+
                 Copyright 2017 React Practice Web Site. All rights reserved. World Wide Web.
 
                 <img src="./images/alum.gif" width="600" />
